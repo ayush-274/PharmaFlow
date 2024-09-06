07ecf155bdd37d535502afacb0984f5d045faee1
meetptl04 <meet06032004@gmail.com>
2024-09-07 00:53:45 +0530

    controller/uesr.controller.js , models/user.model.js , routes/user.route.js added

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userShcema = new mongoose.Schema({
+  fullname: {
+    type : String,
+    required : true
+  },
+  email: {
+    type : String,
+    required : true,
+    unique : true
+  },
+  phonenumber: {
+    type : String,
+    required : true
+  },
+  password: {
+    type : String,
+    required : true
+  },
+  role: {
+    type : String,
+    required : true,
+    enum : ['chemist','distributor','manufacturer'],
+  },
+},{timestamps:true});
+
+export const User = mongoose.model('User',userShcema)
